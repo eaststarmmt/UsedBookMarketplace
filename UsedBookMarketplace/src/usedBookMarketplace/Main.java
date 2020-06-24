a1527f3b4071e6d2e507eb39b2c80078ddb21cf1
DongKyu Kim <aass0903@naver.com>
2020-06-24 16:17:16 +0900

    Login

--- UsedBookMarketplace/src/usedBookMarketplace/Main.java
+++ UsedBookMarketplace/src/usedBookMarketplace/Main.java
@@ -15,27 +15,5 @@ public class Main {
 		System.out.print("Password: ");
 		String password = sc.nextLine();
 		user.Login(id, password);
-	  
-  /*    Connection conn = null;
-    
-   try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      String url = "jdbc:mysql://localhost/usedbookmarketplace?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-      conn = DriverManager.getConnection(url,"root","rlaas7490!");
-      System.out.println("연결 성공");
-    } catch(ClassNotFoundException e) {
-      System.out.println("드라이버 로딩 실패");
-    } catch(SQLException e) {
-      System.out.println("에러: " + e);
-    } finally {
-      try {
-        if(conn != null && !conn.isClosed()) {
-          conn.close();
-        }
-      } catch(SQLException e) {
-    	e.printStackTrace();
-      }
-    }		*/
-    
-  }
+	}
 }
